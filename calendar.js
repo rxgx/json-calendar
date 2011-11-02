d74e0c778f76431eba3ebec7e1518f7b0438327d
Ryan Gasparini <ryan@rxgx.com>
2011-11-02 14:35:37 -0700

    Removed unused methods and moved the defaults option outside of the return object. Defaults object is now a private variable in the closure.

--- calendar.js
+++ calendar.js
@@ -1,20 +1,25 @@
+
 var JsonCalendar = (function() {
 
     var MONTHNAMES = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    var now = new Date;
+    var defaults,
+        nbsp = "&nbsp;",
+        now = new Date;
+
+    defaults = {
+        year            : now.getFullYear(),
+        month           : now.getMonth(),
+        abbreviate      : 2,
+        firstDayOfWeek  : 0,
+        showToday       : true,
+        previousMonth   : nbsp,
+        nextMonth       : nbsp,
+        showOutsideDays : false,
+        otherMonthClass : "month-other"
+    };
 
     return {
-
-        defaults: {
-            year           : now.getFullYear(),
-            month          : now.getMonth(),
-            abbreviate     : 2,
-            firstDayOfWeek : 0,
-            showToday      : true,
-            previousMonth  : "<<",
-            nextMonth      : ">>"
-        },
 
         addDaysToDate: function(date, days) {
             //date.setDate(date.getDate() + days);
@@ -27,12 +32,17 @@ var JsonCalendar = (function() {
             return 32 - new Date(yr, mo - 1, 32).getDate();
         },
 
-        padString: function(string, pad, count) {
-            var parts = [string];
-            for (var i = 0; i < count; i++) {
-                parts.push(pad);
+        getLastDayOfWeek: function(day) {
+            if (day > 0) {
+                return day;
+            } else {
+                return 6;
             }
-            return parts.join();
+        },
+
+        isWeekend: function(date) {
+            var day = date.getDay();
+            return (day === 0 || day === 6);
         },
 
         calendar: function(options) {
@@ -47,16 +57,16 @@ var JsonCalendar = (function() {
                 dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
                 firstDate,
                 firstDayOfWeek,
-                i = 0,
+                i = 1,
                 lastDate,
                 lastDayOfWeek,
                 name,
                 w = 0,
                 week = [];
 
-            for (name in this.defaults) {
+            for (name in defaults) {
                 if (!options.hasOwnProperty(name)) {
-                    options[name] = this.defaults[name];
+                    options[name] = defaults[name];
                 }
             }
 
@@ -65,8 +75,8 @@ var JsonCalendar = (function() {
             firstDate = new Date(options['year'], options['month'], 1);
             lastDate = new Date(options['year'], options['month'] + 1, 0);
 
-            firstDayOfWeek = this.getFirstDayOfWeek(options['firstDayOfWeek']);
-            lastDayOfWeek = this.getLastDayOfWeek(options['firstDayOfWeek']);
+            firstDayOfWeek = options.firstDayOfWeek;
+            lastDayOfWeek = this.getLastDayOfWeek(options.firstDayOfWeek);
 
             // Shift the day names array if the first day of the week isn't Sunday.
             if (options['firstDayOfWeek'] < 0) {
@@ -93,64 +103,53 @@ var JsonCalendar = (function() {
 
             // Loop through week indexes (0..6).
             while (w < 6) {
-
                 d = 0;
-
                 // Loop through the day index (0..6) for each week.
                 while (d < 7) {
-
                     classNames = [];
                     day = {};
-
                     if (i > lastDate.getDate()) {
-
                         // Day of Next Month
+                        if (options.showOutsideDays) {
+                            date = this.addDaysToDate(lastDate, i - lastDate.getDate());
+                        }
                         i += 1;
-                        date = this.addDaysToDate(lastDate, i - lastDate.getDate());
-
+                        classNames.push(options.otherMonthClass);
                     } else if (w === 0 && d < firstDate.getDay()) {
-
                         // Day of Previous Month
-                        date = this.addDaysToDate(firstDate, d - firstDate.getDay());
-                        classNames.push(options['other_month_class']);
-
+                        if (options.showOutsideDays) {
+                            date = this.addDaysToDate(firstDate, d - firstDate.getDay());
+                        }
+                        classNames.push(options.otherMonthClass);
                     } else {
-
                         // Day of Current Month
-                        i += 1;
+                        classNames.push("month-day");
                         date = new Date(firstDate.getFullYear(), firstDate.getMonth(), i);
-
-                        classNames.push(options['day_class']);
-
+                        i += 1;
                         if (date.toDateString() === now.toDateString() && options['show_today']) {
                             classNames.push("today");
                         }
-
                     }
-
-                    if (this.isWeekend(date)) {
-                        classNames.push("weekend-day");
+                    if (date && date.getDate) {
+                        if (this.isWeekend(date)) {
+                            classNames.push("weekend-day");
+                        }
+                        day.className = classNames.join(" ");
+                        day.id = "day" + date.getDate();
+                        day.day = date.getDate();
+                        day.date = date;
+                        day.month = date.getMonth();
+                        day.year = date.getFullYear();
+                        date = undefined;
+                    } else {
+                        day = { date: nbsp, day: nbsp };
                     }
-
-                    // Expose these properties as API.
-                    day.className = classNames.join(" ");
-                    day.id = "day" + date.getDate();
-                    day.day = date.getDate();
-                    day.date = date;
-                    day.month = date.getMonth();
-                    day.year = date.getFullYear();
-
                     week.push(day);
-
                     d += 1;
-
                 }
-
                 data.weeks.push(week);
                 week = [];
-
                 w += 1;
-
             }
 
             data.nextMonth = options.nextMonth;
@@ -159,39 +158,8 @@ var JsonCalendar = (function() {
 
             return data;
 
-        },
-
-
-        getFirstDayOfWeek: function(day) {
-            return day;
-        },
-
-        getLastDayOfWeek: function(day) {
-            if (day > 0) {
-                return day - 1;
-            } else {
-                return 6;
-            }
-        },
-
-        getDaysBetween: function(first, second) {
-            if (first > second) {
-                return second + (7 - first);
-            } else {
-                return second - first;
-            }
-        },
-
-        getBeginningOfWeek: function(date, start) {
-            start = start || 1;
-            var delta = this.getDaysBetween(start, date.getDay());
-            return this.addDaysToDate(date, -delta);
-        },
-
-        isWeekend: function(date) {
-            var day = date.getDay();
-            return (day === 0 || day === 6);
         }
+
     };
 
 })();
