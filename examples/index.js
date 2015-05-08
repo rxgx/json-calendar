c80c497e0c1cdb481fda34a6fccb117b7b93bad9
Ryan Gasparini <ryan@rxgx.com>
2015-05-08 14:14:47 -0700

    Moved index file to examples

---
+++ examples/index.js
@@ -0,0 +1,6 @@
+
+var Calendar = require('./src/calendar');
+var test = new Calendar();
+
+console.log("Weeks this month: `test.weeks.length`");
+console.log(test.monthNames);
--- index.js
+++
@@ -1,4 +0,0 @@
-
-var Calendar = require('./src/calendar');
-var test = new Calendar();
-console.log(test.weeks.length);
