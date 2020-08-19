8de1b0211104a32e4f488d1c011a70d3c7065fc8
Ryan Gasparini <ryan@rxgx.com>
2020-08-19 12:07:34 -0700

    use standardx

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    'jest/globals': true,
-  },
-  extends: [
-    'airbnb-base',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:import/typescript',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    jest: true,
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -23,8 +11,8 @@ module.exports = {
     sourceType: 'module',
     tsconfigRootDir: __dirname,
   },
-  plugins: ['jest', 'prettier'],
   rules: {
-    'prettier/prettier': 'error',
-  },
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error"
+  }
 };
